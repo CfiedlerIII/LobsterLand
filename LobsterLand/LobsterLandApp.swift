f3a772f9eb8b26ab11e24e7645e8c91c5419eb0a
Charles Fiedler <charlesfiedleriii@gmail.com>
2024-11-08 13:38:04 -0800

    Add files via upload

---
+++ LobsterLand/LobsterLandApp.swift
@@ -0,0 +1,17 @@
+//
+//  LobsterLandApp.swift
+//  LobsterLand
+//
+//  Created by Charles Fiedler on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LobsterLandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
